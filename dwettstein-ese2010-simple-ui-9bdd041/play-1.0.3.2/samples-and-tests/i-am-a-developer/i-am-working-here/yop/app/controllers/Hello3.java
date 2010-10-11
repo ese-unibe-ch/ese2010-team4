ff86afdd44457918d682b0814732599590a9502f
Juerg Weber <juerg.weber@students.unibe.ch>
2010-10-11 10:27:16 +0200

    cleaned the repo

--- dwettstein-ese2010-simple-ui-9bdd041/play-1.0.3.2/samples-and-tests/i-am-a-developer/i-am-working-here/yop/app/controllers/Hello3.java
+++
@@ -1,7 +0,0 @@
-package controllers;
-import play.mvc.*;
-public class Hello3 extends Application {
-  public static void hello() {
-      renderText("Hello");
-  }
-}
---
+++ qa/app/controllers/Application.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.util.List;
+
+import play.data.validation.*;
+import models.*;
+import play.mvc.*;
+
+/**A controller for the index.
+ * 
+ * @author dwettstein
+ * 
+ */
+public class Application extends Controller {
+	
+    public static void index() {
+    	Question lastQuestion = Question.find("order by timestamp desc").first();
+        List<Question> questions = Question.find("order by voting desc").fetch();
+        render(lastQuestion, questions);
+    }
+    
+    public static void show(Long id) {
+        Question question = Question.findById(id);
+        render(question);
+    }
+    
+    public static void createUser(String fullname, String email, String password){
+    	
+    	
+    	
+    }
+
+}
