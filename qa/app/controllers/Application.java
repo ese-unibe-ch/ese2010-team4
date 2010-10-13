f8f441a21807ba5d5da4c61f872de95627a4318a
Juerg Weber <juerg.weber@students.unibe.ch>
2010-10-13 14:08:07 +0200

    working files

--- qa/app/controllers/Application.java
+++ qa/app/controllers/Application.java
@@ -14,52 +14,53 @@ import play.mvc.Controller;
  */
 public class Application extends Controller {
 
-	 public static void index() {
-		 Question lastQuestion = Question.find("order by timestamp desc")
-		 .first();
-		 List<Question> questions = Question.find("order by voting desc")
-		 .fetch();
-		 render(lastQuestion, questions);
-		 }
+	// comment to push
+	public static void index() {
+		Question lastQuestion = Question.find("order by timestamp desc")
+				.first();
+		List<Question> questions = Question.find("order by voting desc")
+				.fetch();
+		render(lastQuestion, questions);
+	}
 
-		 public static void show(Long id) {
-		 Question question = Question.findById(id);
-		 render(question);
-		 }
+	public static void show(Long id) {
+		Question question = Question.findById(id);
+		render(question);
+	}
 
-		 public static void createUser(String message) {
+	public static void createUser(String message) {
 
-		 render(message);
+		render(message);
 
-		 }
+	}
 
-		 public static void addUser(String fullname, String email, String password,
-		 String password2) {
+	public static void addUser(String fullname, String email, String password,
+			String password2) {
 
-		 String message;
-		 User user = User.find("byEmail", email).first();
+		String message;
+		User user = User.find("byEmail", email).first();
 
-		 if (!password.equals(password2)) {
-		 message = "the password's aren't the same";
+		if (!password.equals(password2)) {
+			message = "the password's aren't the same";
 
-		 }
+		}
 
-		 else if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
-		 message = "you fogot one or more gap's";
+		else if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
+			message = "you fogot one or more gap's";
 
-		 }
+		}
 
-		 else if (user != null && user.email.equals(email)) {
+		else if (user != null && user.email.equals(email)) {
 
-		 message = "user allready exists";
-		 }
+			message = "user allready exists";
+		}
 
-		 else {
-		 new User(fullname, email, password).save();
-		 message = "Hello, " + fullname + ", please log in";
-		 }
+		else {
+			new User(fullname, email, password).save();
+			message = "Hello, " + fullname + ", please log in";
+		}
 
-		 createUser(message);
-		 }
+		createUser(message);
+	}
 
 }
