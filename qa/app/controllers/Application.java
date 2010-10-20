664cd43f2f3c037a1e6b36e334dda52aee5df106
Juerg Weber <juerg.weber@students.unibe.ch>
2010-10-20 10:37:45 +0200

    fixed the pw bug in Application.createUser, i going to refactor the methot later

--- qa/app/controllers/Application.java
+++ qa/app/controllers/Application.java
@@ -32,42 +32,44 @@ public class Application extends Controller {
 
 	/**
 	 * Show.
-	 *
-	 * @param id the id
+	 * 
+	 * @param id
+	 *            the id
 	 */
 	public static void show(Long id) {
-		
+
 		Question question = Question.find("byId", id).first();
-		
+
 		boolean abletochoose = false;
 		boolean abletovote = false;
 		boolean isvalid = false;
-		
-		if(!Security.isConnected()){
-			
+
+		if (!Security.isConnected()) {
+
 			render(question, isvalid, abletochoose, abletovote);
 		}
-		
-		else{
+
+		else {
 			User user = User.find("byEmail", Security.connected()).first();
-			
+
 			abletochoose = user.isAbleToChoose(id);
 			abletovote = user.isAbleToVote(id);
-			isvalid	= user.hasTimeToChange(id);
-			
+			isvalid = user.hasTimeToChange(id);
+
 			System.out.println();
 			System.out.println(abletochoose);
 			System.out.println(abletovote);
 			System.out.println(isvalid);
-			
+
 			render(question, isvalid, abletochoose, abletovote);
 		}
 	}
 
 	/**
 	 * Creates the user.
-	 *
-	 * @param message the message
+	 * 
+	 * @param message
+	 *            the message
 	 */
 	public static void createUser(String message) {
 
@@ -77,11 +79,15 @@ public class Application extends Controller {
 
 	/**
 	 * Adds the user.
-	 *
-	 * @param fullname the fullname
-	 * @param email the email
-	 * @param password the password
-	 * @param password2 the password2
+	 * 
+	 * @param fullname
+	 *            the fullname
+	 * @param email
+	 *            the email
+	 * @param password
+	 *            the password
+	 * @param password2
+	 *            the password2
 	 */
 	public static void addUser(String fullname, String email, String password,
 			String password2) {
@@ -89,13 +95,16 @@ public class Application extends Controller {
 		String message;
 		User user = User.find("byEmail", email).first();
 
-		if (!password.equals(password2)) {
+		if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
+			message = "you forgot one or more gap's";
+		}
+
+		else if (!password.equals(password2)) {
 			message = "the password's aren't the same";
 		}
 
-		if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
-			message = "you forgot one or more gap's";
-
+		else if (password.length() < 6) {
+			message = "your password must be 6 singns or longer";
 		}
 
 		else if (user != null && user.email.equals(email)) {
