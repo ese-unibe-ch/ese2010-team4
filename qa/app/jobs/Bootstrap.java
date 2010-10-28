15385aa0302d0b10539eb49383adc2d702ca8040
Juerg Weber <juerg.weber@students.unibe.ch>
2010-10-28 09:59:27 +0200

    bootstrap

--- qa/app/jobs/Bootstrap.java
+++ qa/app/jobs/Bootstrap.java
@@ -2,8 +2,10 @@ package jobs;
 
 import models.User;
 import play.jobs.Job;
+import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
 
+@OnApplicationStart
 public class Bootstrap extends Job {
 
 	public void doJob() {
