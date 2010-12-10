358a40185a00a43107d335c24b577b4c04257183
Juerg Weber <juerg.weber@students.unibe.ch>
2010-12-10 11:11:19 +0100

    add tabs

--- qa/public/javascripts/userprofile.js
+++ qa/public/javascripts/userprofile.js
@@ -4,13 +4,38 @@ $(function() {
 		loadGraph();
 	
 	});
+	$('div.profileTab').hide();
+	$('div#userInfo').show();
+	$('a[href=#userInfo]').addClass("active");
+	$('a.button[rel=tab]').click(function(){
+		$('a.button[rel=tab]').removeClass("active");
+		$(this).addClass("active");
+		var a = this.hash.substr(1);
+		
+		$('div.profileTab').hide();		
+		$('div.profileTab').each(function(){
+			
+			if(this.id==a){				
+				$(this).show();
+				
+				if(this.id=="userActivities"){
+					
+					loadGraph();
+				}					
+			}
+			
+		});
+		return false;
+		
+	});
 
 });
 
 function loadGraph() {
-	if(!$("#graph")[0].loaded) {
+	if(!$("div#graph")[0].loaded) {
+		
 		jQuery.getJSON( graphData({id: userid}), displayGraph);
-		$("#graph")[0].loaded = true;
+		$("div#graph")[0].loaded = true;
 	}
 }
 
